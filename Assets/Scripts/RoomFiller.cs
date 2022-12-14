a9487595f633e1846efa92bd14b631532e56cd5b
Harrison Giger <harrisongiger21@gmail.com>
2022-12-14 15:31:39 -0800

    Jesse said some words

---
+++ Assets/Scripts/RoomFiller.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RoomFiller
+{
+    public GameObject roomModel;
+    public int NorthType;
+    public int EastType;
+    public int SouthType;
+    public int WestType;
+    public List<int> NorthIndex;
+    public List<int> EastIndex;
+    public List<int> SouthIndex;
+    public List<int> WestIndex;
+
+
+    // construct class based on inputs
+    RoomFiller(GameObject inRoomModel, int inNorth, int inEast, int inSouth, int inWest)
+    {
+        roomModel = inRoomModel;
+        NorthType = inNorth;
+        EastType = inEast;
+        SouthType = inSouth;
+        WestType = inWest;
+    }
+
+    // run this once for each item once all types are added
+    public void CalcLists(ref List<RoomFiller> optionsIn)
+    {
+        // push all the possible options for each direction to their respective lists
+        for(int i = 0; i < optionsIn.Count; i++)
+        {
+            if (optionsIn[i].NorthType == SouthType)
+            {
+                SouthIndex.Append(i);
+            }
+            if (optionsIn[i].EastType == WestType)
+            {
+                WestIndex.Append(i);
+            }
+            if (optionsIn[i].SouthType == NorthType)
+            {
+                NorthIndex.Append(i);
+            }
+            if (optionsIn[i].WestType == EastType)
+            {
+                EastIndex.Append(i);
+            }
+        }
+    }
+}
--- Assets/Scripts/roomGenerator.cs
+++ Assets/Scripts/roomGenerator.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class roomGenerator : MonoBehaviour
 {
 
-    [SerializeField]
-    GameObject startingRoom;
-    // door at +z, -x
-    [SerializeField]
-    GameObject cornerRoom;
-    // door at -x
-    [SerializeField]
-    GameObject oneDoorRoom;
-    // door at -x, +x
-    [SerializeField]
-    GameObject twoDoorRoom;
-    // door at -x, -z, +z
-    [SerializeField]
-    GameObject threeDoorRoom;
-    // door in all directions
-    [SerializeField]
-    GameObject fourDoorRoom;
+    //[SerializeField]
+    //GameObject startingRoom;
+    //// door at +z, -x
+    //[SerializeField]
+    //GameObject cornerRoom;
+    //// door at -x
+    //[SerializeField]
+    //GameObject oneDoorRoom;
+    //// door at -x, +x
+    //[SerializeField]
+    //GameObject twoDoorRoom;
+    //// door at -x, -z, +z
+    //[SerializeField]
+    //GameObject threeDoorRoom;
+    //// door in all directions
+    //[SerializeField]
+    //GameObject fourDoorRoom;
 
-
+    
 
     // Start is called before the first frame update
     void Start()
